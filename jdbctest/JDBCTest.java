59d993df683ef442fb4de9c967577592e8297c4a
Fusayuki Minamoto <fminamot@redhat.com>
2023-08-04 04:52:34 -0400

    Modified JDBC java test

--- jdbctest/JDBCTest.java
+++ jdbctest/JDBCTest.java
@@ -6,31 +6,31 @@ import java.sql.Statement;
 
 public class JDBCTest {
 
-     public static void main(String[] args) {
-	  System.out.println("JDBC test");
+    public static void main(String[] args) {
+        System.out.println("JDBC test");
 
-          Connection connection;
+        Connection connection;
 
-          try {
-               connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost/items?user=user1&password=mypa55");
-               try {
-                    Statement statement = connection.createStatement();
-                    ResultSet rs = statement.executeQuery("SELECT * FROM Projects" );
+        try {
+             connection = DriverManager.getConnection(
+                  "jdbc:mysql://localhost/items?user=user1&password=mypa55");
+             try {
+                 Statement statement = connection.createStatement();
+                 ResultSet rs = statement.executeQuery("SELECT * FROM Projects" );
 
-                    while( rs.next() ) {
-                         System.out.println(
-		           "Record=(" + rs.getInt(1) + "," + rs.getString(2) + "," + rs.getString(3) + ")"
-                         );
-                    }
-               }
-               finally {
-                    connection.close();
-               }
+                 while( rs.next() ) {
+                     System.out.println(
+                         "Record=(" + rs.getInt(1) + "," + rs.getString(2) + "," + rs.getString(3) + ")"
+                     );
+                  }
+             }
+             finally {
+                 connection.close();
+             }
 
-          } catch (SQLException ex) {
-               System.out.println(ex);
-          }
-     }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
 }
 
