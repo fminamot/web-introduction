f59bd47f399f55b14a327b10135c1880727cbae2
Fusayuki Minamoto <fminamot@redhat.com>
2023-08-04 04:09:36 -0400

    Renamed JDBC test class

--- jdbctest/JDBCTest.java
+++ jdbctest/JDBCTest.java
@@ -4,23 +4,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class MySQLTest {
+public class JDBCTest {
 
      public static void main(String[] args) {
           Connection conn;
-          Statement stat;
-          ResultSet rs;
 
           try {
                conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost/items?user=user1&password=mypa55");
                try {
-                    stat = conn.createStatement();
-                    rs = stat.executeQuery("SELECT * FROM Projects" );
+                    Statement statement = conn.createStatement();
+                    ResultSet rs = statement.executeQuery("SELECT * FROM Projects" );
 
                     while( rs.next() ) {
-                         System.out.format(
-                              "%d, %s, %s\n", rs.getInt(1), rs.getString(2), rs.getString(3)
+                         System.out.println(
+		           "Record=(" + rs.getInt(1) + "," + rs.getString(2) + "," + rs.getString(3) + ")"
                          );
                     }
                }
@@ -29,9 +27,7 @@ public class MySQLTest {
                }
 
           } catch (SQLException ex) {
-               System.out.println("SQLException: " + ex.getMessage());
-               System.out.println("SQLState: " + ex.getSQLState());
-               System.out.println("VendorError: " + ex.getErrorCode());
+               System.out.println(ex);
           }
      }
    }
